cc3cda7c733472a853f2ef73a2f0866211b62132
PD <dave.sfsu@gmail.com>
2017-01-28 18:41:35 -0800

    Added the tableView and the tableViewCell with the basic outine for the individual businesses

---
+++ Yelp/BusinessCell.swift
@@ -0,0 +1,24 @@
+//
+//  BusinessCell.swift
+//  Yelp
+//
+//  Created by Poojan Dave on 1/28/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class BusinessCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
